87dd979dfacaae454e3804c0b75a17ca2eb3b697
Xinbo Tian <32756076+xinbotian@users.noreply.github.com>
2024-08-26 21:35:20 -0400

    create components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const home = () => {
   return (
-    <div>home</div>
+    <section className='home'></section>
   )
 }
 
---
+++ components/HeaderBox.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeaderBox = ({ type = "title", title, subtext, user}) => {
+  return (
+    <div>HeaderBox</div>
+  )
+}
+
+export default HeaderBox
